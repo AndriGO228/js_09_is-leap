534ff23d3bfc34cd9cbd608240e445b1c0610e43
andriGo <afomin129@gmail.com>
2022-11-17 00:40:27 +0200

    default

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function isLeap(year) {
-	if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+	if (year % 4 === 0 && year % 100 !== 0) {
+		return true;
+	} else if (year % 400 === 0) {
 		return true;
 	} else {
 		return false;
 	}
 }
+
 module.exports = isLeap;
