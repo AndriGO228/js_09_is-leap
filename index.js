2a50441c9ce28a15c2c61850efa9348a0062fa21
andriGo <afomin129@gmail.com>
2022-11-16 05:56:00 +0200

    default

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// зачем тогда вообще if? year % 4 === 0 && year % 100 !== 0 && year % 400 === 0 - само по себе вернёт true / false.
 function isLeap(year) {
 	if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
 		return true;
-	}else {
+	} else {
 		return false;
 	}
-} module.exports = isLeap;
+
+}
+module.exports = isLeap;
